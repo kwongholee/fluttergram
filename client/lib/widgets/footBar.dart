1b9a4d4536c9497ffdb22b5484b45e3ff283661e
kwongholee <ghlee9883@gmail.com>
2024-04-15 20:08:51 +0900

    24.04.15 client - refactoring: 중복되는 bottomnavigationbar 위젯 따로 빼기

---
+++ client/lib/widgets/footBar.dart
@@ -0,0 +1,32 @@
+import 'package:client/pages/home.dart';
+import 'package:client/pages/profile.dart';
+import 'package:client/pages/search.dart';
+import 'package:client/stores/userProvider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class FootBar extends StatelessWidget {
+  const FootBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      onTap: (i) {
+        if(i == 0) {
+          Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false,);
+        } else if(i == 1) {
+          Navigator.pushNamed(context, '/search');
+        } else {
+          Navigator.pushNamed(context, '/profile');
+        }
+      },
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+        BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: 'Account'),
+      ],
+    );
+  }
+}
