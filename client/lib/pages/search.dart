3bfb56ceac2c446b61b6402839fbf302d8803092
kwongholee <ghlee9883@gmail.com>
2024-04-15 13:53:08 +0900

    24.04.15 client - feat: search page 구현

--- client/lib/pages/search.dart
+++ client/lib/pages/search.dart
@@ -2,13 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:client/stores/userProvider.dart';
 import 'package:provider/provider.dart';
 
-class Search extends StatelessWidget {
+class Search extends StatefulWidget {
   const Search({Key? key}) : super(key: key);
+
+  @override
+  State<Search> createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  String keyword = "";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(),
+      appBar: AppBar(title: Text("Search Page"),),
+      body: CustomScrollView(slivers: [
+        SliverToBoxAdapter(child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          SizedBox(width: 300, child: TextField(onChanged: (e) {setState(() {
+            keyword = e;
+          });})),
+          IconButton(onPressed: () {}, icon: Icon(Icons.search))
+        ])),
+        SliverGrid(delegate: SliverChildBuilderDelegate((c,i) => Container(child: Image.asset('assets/jiwoo.jpg')), childCount: 15),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3))
+      ]),
       bottomNavigationBar: context.watch<userProvider>().direction == "up" ? BottomNavigationBar(
         showSelectedLabels: false,
         showUnselectedLabels: false,
