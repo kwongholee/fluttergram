9d95971855d5f5fd265d473d1ca39aa2c5040ae2
kwongholee <ghlee9883@gmail.com>
2024-04-17 07:14:20 +0900

    24.04.17 client - feat: feed 좋아요, 각 피드당 설정 기능

--- client/lib/widgets/feed.dart
+++ client/lib/widgets/feed.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
-class Feed extends StatelessWidget {
+class Feed extends StatefulWidget {
   const Feed({Key? key}) : super(key: key);
 
+  @override
+  State<Feed> createState() => _FeedState();
+}
+
+class _FeedState extends State<Feed> {
   final writer = "이광호";
-  final like = 0;
+  var like = 0;
+  bool isPressedLike = false;
   final content = "지우 개예뻐 미친~~";
 
   @override
@@ -13,10 +19,26 @@ class Feed extends StatelessWidget {
         padding: EdgeInsets.all(10),
         child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
           FeedHeader(writer: writer),
-          Image.asset('assets/jiwoo.jpg'),
+          InkWell(onDoubleTap: () {setState(() {
+            isPressedLike = !isPressedLike;
+            if(isPressedLike) {
+              like++;
+            } else {
+              like--;
+            }
+          });}, child: Image.asset('assets/jiwoo.jpg'),),
           Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-            Icon(Icons.favorite_border_outlined),
-            Icon(Icons.send)
+            IconButton(onPressed: () {
+              setState(() {
+                isPressedLike = !isPressedLike;
+                if(isPressedLike) {
+                  like++;
+                } else {
+                  like--;
+                }
+              });
+            }, icon: isPressedLike ? Icon(Icons.favorite) : Icon(Icons.favorite_border_outlined)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.send))
           ]),
           Text("좋아요: ${like.toString()}"),
           Text(content)
@@ -34,9 +56,46 @@ class FeedHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(padding: EdgeInsets.fromLTRB(0, 0, 0, 10), child: Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-        Row(children: [Icon(Icons.account_circle), Text("${writer}")]),
-        Icon(Icons.more_vert)
+        Row(children: [Icon(Icons.account_circle), Text(writer)]),
+        IconButton(icon: Icon(Icons.more_vert), onPressed: () {showDialog(context: context, barrierDismissible: true, builder: (context) {
+          return FeedDialog();
+        });})
       ],)
     );
   }
 }
+
+class FeedDialog extends StatefulWidget {
+  FeedDialog({Key? key}) : super(key: key);
+
+  @override
+  State<FeedDialog> createState() => _FeedDialogState();
+}
+
+class _FeedDialogState extends State<FeedDialog> {
+  var dialogList = [
+    {"name": "edit", "icon": Icon(Icons.edit), "route": "/feed/edit"},
+    {"name": "delete", "icon": Icon(Icons.delete), "route": "/feed/delete"}
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(child: SizedBox(
+      height: 90,
+      child: ListView.builder(itemCount: dialogList.length, itemBuilder: (c,i) {
+        Map<String, dynamic> item = dialogList[i];
+        return InkWell(
+            onTap: () {},
+            child: Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(border: Border(bottom: BorderSide(color: Colors.black))),
+              child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+                Text(item["name"]),
+                item["icon"]
+              ],),
+            )
+        );}),
+    ));
+  }
+}
+
