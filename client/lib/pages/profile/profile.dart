4446985f798f45e078506062b5dde3176030dd85
kwongholee <ghlee9883@gmail.com>
2024-04-20 12:08:05 +0900

    24.04.20 client - feat: profile edit setting page

--- client/lib/pages/profile/profile.dart
+++ client/lib/pages/profile/profile.dart
@@ -1,3 +1,4 @@
+import 'package:client/stores/feedListProvider.dart';
 import 'package:client/widgets/footBar.dart';
 import 'package:client/widgets/profileHeader.dart';
 import 'package:flutter/material.dart';
@@ -10,13 +11,20 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("${context.watch<userProvider>().id}'s Profile Page"), actions: [IconButton(onPressed: () {Navigator.pushNamed(context, "/profile/settings");}, icon: Icon(Icons.settings))]),
+      appBar: AppBar(title: Text("${context.watch<UserProvider>().userInfo["id"]}'s Profile Page"), actions: [IconButton(onPressed: () {Navigator.pushNamed(context, "/profile/settings");}, icon: Icon(Icons.settings))]),
       body: CustomScrollView(slivers: [
         SliverToBoxAdapter(child: ProfileHeader()),
-        SliverGrid(delegate: SliverChildBuilderDelegate((c,i) => Container(child: Image.asset('assets/jiwoo.jpg')), childCount: 4),
+        SliverGrid(delegate: SliverChildBuilderDelegate((c,i) =>
+            InkWell(
+              onTap: () {
+                Navigator.pushNamed(context, "/feed/detail/${i.toString()}");
+              },
+              child: Container(child: Image.asset('assets/jiwoo.jpg'))
+            ),
+            childCount: context.watch<UserFeedListProvider>().feedList.length),
             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3))
       ]),
-      bottomNavigationBar: context.watch<userProvider>().direction == "up" ? FootBar() : null
+      bottomNavigationBar: context.watch<UserProvider>().direction == "up" ? FootBar() : null
     );
   }
 }
--- client/lib/pages/profile/settingProfileEdit.dart
+++ client/lib/pages/profile/settingProfileEdit.dart
@@ -8,11 +8,30 @@ class ProfileEdit extends StatefulWidget {
 }
 
 class _ProfileEditState extends State<ProfileEdit> {
+  var settingList = [
+    {"name": "id 고치기", "route": "/profile/setting/modification/id"},
+    {"name": "이름 고치기", "route": "/profile/setting/modification/name"},
+    {"name": "상태메세지 고치기", "route": "/profile/setting/modification/introduce"}
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(leading: IconButton(icon: Icon(Icons.arrow_back), onPressed: () {Navigator.pop(context);}), title: Text("Profile Edit Page")),
-      body: Container(child: Text("profile edit page"),),
+      body: ListView.builder(
+          itemCount: 3,
+          itemBuilder: (c, i) => InkWell(
+            onTap: () {Navigator.pushNamed(context, settingList[i]["route"]!);},
+            child: Container(
+              decoration: BoxDecoration(border: Border(bottom: BorderSide(color: Colors.black))),
+              padding: EdgeInsets.all(20),
+              child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                Icon(Icons.badge),
+                Text(settingList[i]["name"]!)
+              ]),
+            )
+          )
+      )
     );
   }
 }
