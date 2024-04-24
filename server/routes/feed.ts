55db109b19cad72b0f27d5a49a7a031aed057e44
kwongholee <ghlee9883@gmail.com>
2024-04-24 21:00:43 +0900

    24.04.24 server - feat: route edit

--- server/routes/feed.ts
+++ server/routes/feed.ts
@@ -1,9 +1,8 @@
 const router = require('express').Router();
 import { Request, Response } from "express";
+var addFeed = require('../controller/addFeed.controller');
 
-router.post('/new', (req: Request, res: Response) => {
-  res.send('post new feed');
-})
+router.post('/new', (req: Request, res: Response) => addFeed(req, res));
 
 router.put('/original', (req: Request, res: Response) => {
   res.send('edit original feed');
--- server/routes/info.ts
+++ server/routes/info.ts
@@ -1,5 +1,8 @@
 const router = require('express').Router();
+const mongoose = require('mongoose');
 import { Request, Response } from 'express';
+var feedModel = require('../model/feed');
+var ObjectId = mongoose.Types.ObjectId;
 
 router.get('/user', (req: Request, res: Response) => {
   res.send("user info");
@@ -14,7 +17,35 @@ router.get('/message', (req: Request, res: Response) => {
 })
 
 router.get('/feed', (req: Request, res: Response) => {
-  res.send("feed info");
+  var id = req.query.id;
+  feedModel.findOne({_id: new ObjectId(id)}, (err: Error, feed: any) => {
+    if(err || !feed) {
+      console.log(err);
+      return res.status(400);
+    }
+    return res.status(200).json(feed);
+  })
+})
+
+router.get('/feed/main', (req: Request, res: Response) => {
+  feedModel.find({}, (err: Error, feeds: any) => {
+    if(err) {
+      console.log(err);
+      return res.status(400);
+    }
+    return res.status(200).json(feeds);
+  })
+})
+
+router.get('/feed/user', (req: Request, res: Response) => {
+  var user = req.query.user;
+  feedModel.find({writer: user}, (err: Error, feeds: any) => {
+    if(err) {
+      console.log(err);
+      return res.status(400);
+    }
+    return res.status(200).json(feeds);
+  })
 })
 
 module.exports = router;
