b838addc3be75db274508716a932483a2aef88e2
kwongholee <ghlee9883@gmail.com>
2024-04-26 19:12:12 +0900

    24.04.26 server - chore: chanage connectNeo4j module

--- server/config/connectNeo4j.ts
+++ server/config/connectNeo4j.ts
@@ -4,15 +4,17 @@ const connectNeo4j = async () => {
   const URI = process.env.NEO4J_URI;
   const neo4jUser = process.env.NEO4J_USERNAME;
   const neo4jPassword = process.env.NEO4J_PASSWORD;
-  let driver;
 
   try {
-    driver = neo4j.driver(URI, neo4j.auth.basic(neo4jUser, neo4jPassword));
-    const serverInfo = await driver.getServerInfo();
+    const driver = neo4j.driver(URI, neo4j.auth.basic(neo4jUser, neo4jPassword));
+    const session = driver.session();
+    const serverInfo = await driver.verifyConnectivity();
     console.log('Success to connect with Neo4j');
-    console.log(serverInfo);
-  } catch(err) {
+    await session.close();
+    return { driver };
+  } catch (err) {
     console.log(err);
+    throw err;
   }
 }
 
