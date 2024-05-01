6941b1e07174d001a189b28248c62dc1541bccc0
kwongholee <ghlee9883@gmail.com>
2024-05-01 18:19:04 +0900

    24.05.01 server - feat: add user follower, chatroom, message routes

--- server/routes/info.ts
+++ server/routes/info.ts
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const mongoose = require('mongoose');
 import { Request, Response } from 'express';
 var feedModel = require('../model/feed');
+var chatModel = require('../model/chat');
+var chatroomModel = require('../model/chatroom');
 var ObjectId = mongoose.Types.ObjectId;
 var connectNeo4j = require('../config/connectNeo4j');
 
@@ -23,8 +25,24 @@ router.get('/user', async (req: Request, res: Response) => {
   }
 })
 
-router.get('/user/follower', (req: Request, res: Response) => {
-  res.send("user follower info");
+router.get('/user/follower', async (req: Request, res: Response) => {
+  var id = req.query.id;
+  try {
+    let data;
+    const {driver} = await connectNeo4j();
+    let {records, summary} = await driver.executeQuery(
+      'MATCH (u: User {id: $id})-[:Follow]->(f) RETURN f',
+      {id: id},
+      {database: 'neo4j'}
+    );
+    for(let record of records) {
+      data = (record.get("f"));
+      return res.status(200).json(data);
+    }
+  } catch(err) {
+    console.log(err);
+    return res.status(400);
+  }
 })
 
 router.get('/user/follow/check', async (req: Request, res: Response) => {
@@ -45,14 +63,32 @@ router.get('/user/follow/check', async (req: Request, res: Response) => {
   }
 })
 
-router.get('/message', (req: Request, res: Response) => {
-  res.send("chatroom info");
+router.get('/chatroom', async (req: Request, res: Response) => {
+  var id = req.query.id;
+  try {
+    const result = await chatroomModel.find({$or: [{user1: id}, {user2: id}]}).exec();
+    return res.status(200).json(result);
+  } catch(err) {
+    console.log(err);
+    return res.status(400);
+  }
+})
+
+router.get('/chatroom/message', async (req: Request, res: Response) => {
+  var room = req.query.chatroom, limit: any = req.query.limit, skip: any = req.query.skip;
+  try {
+    const result = await chatModel.find({chatroom: room}).skip(parseInt(skip)).limit(parseInt(limit)).exec();
+    return res.status(400).json(result);
+  } catch(err) {
+    console.log(err);
+    return res.status(400);
+  }
 })
 
 router.get('/feed', async (req: Request, res: Response) => {
   try {
     const result = await feedModel.findOne({_id: new ObjectId(req.query.id)}).exec();
-    res.status(200).json(result);
+    return res.status(200).json(result);
   }
   catch(err) {
     console.log(err);
